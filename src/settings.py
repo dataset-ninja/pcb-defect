b9d1e2fe9189bac01f43bcb7d90526a21546254b
erastov-alex <erastov.alex@gmail.com>
2023-11-10 20:09:44 +0300

    upd settings

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://paperswithcode.com/paper/a-pcb-dataset-for-defects-detection-and"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 8468893
+PREVIEW_IMAGE_ID: int = 8505043
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/pcb-defect"
